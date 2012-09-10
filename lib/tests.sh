bfefa3ec2bb42620bb2d4a69ee291390c1f0b6d5
D. Elmo Peele <depeele@tycho.ncsc.mil>
2012-09-10 09:13:23 -0400

    If there is no '.expected' file, skip the test.

--- lib/tests.sh
+++ lib/tests.sh
@@ -3,16 +3,20 @@
 for test in tests/*.js; do
      dir=`dirname  "${test}"`
     base=`basename "${test}" '.js'`
+     exp="${dir}/${base}.expected"
+    if [ ! -f "${exp}" ]; then
+        continue;
+    fi
 
     /bin/echo -n "Testing '${base}'... "
     node "${test}" > test.res
-    diff -q test.res "${dir}/${base}.expected" > /dev/null 2>&1
+    diff -q test.res "${exp}" > /dev/null 2>&1
     rc=$?
 
     if [ ${rc} -ne 0 ]; then
         echo "FAILED"
     else
         echo "pass"
-		rm -f test.res
+        rm -f test.res
     fi
 done
